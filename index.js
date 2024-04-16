6d822e064e3bf4bca55117e1ea80db9f79550ee0
NewManuel <100541053+NewManuel@users.noreply.github.com>
2024-04-16 18:15:28 -0500

    File - index..js file creation and prompt function

--- index.js
+++ index.js
@@ -24,3 +24,44 @@ const designQnA = [
         message: "What color would you like your text to be (enter a name or the hexidecimal)?",
     },
 ];
+
+function intialize() {
+    inquirer.prompt(designQnA).then((answers) => {
+        console.log(answers);
+        const inputShape = answers;
+        fileCreate("./distro/logo.svg", inputShape);
+    });
+};
+
+function fileCreate(fileName, answers) {
+    let svgString = "";
+    svgString =
+        '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+    svgString += "<g>";
+
+    let shapeChoice;
+
+    if (answers.shape === "Triangle") {
+        shapeChoice = new Triangle();
+        svgString += `<polygon points="150, 18 244, 182 56, 182" fill="${answers.color}"/>`;
+    } else if (answers.shape === "Square") {
+        shapeChoice = new Square();
+        svgString += `<rect x="73" y="40" width="160" height="160" fill="${answers.color}"/>`;
+    } else {
+        shapeChoice = new Circle();
+        svgString += `<circle cx="150" cy="115" r="80" fill="${answers.color}"/>`;
+    }
+    console.log(answers.shape);
+    const text = answers.text;
+    const capitalText = text.toUpperCase();
+
+    svgString += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${capitalText}</text>`;
+
+    svgString += "</g>";
+
+    svgString += "</svg>";
+
+    fs.writeFile(fileName, svgString, (err) => {
+        err ? console.log(err) : console.log("Generated logo.svg");
+    });
+};
